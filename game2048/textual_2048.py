c9f14b58e8948c041dca5b61f80d371d49abe79e
safae issaoui <safae.issaoui2020@gmail.com>
2021-11-09 11:29:42 +0100

    Tests

--- game2048/textual_2048.py
+++ game2048/textual_2048.py
@@ -1,2 +1,26 @@
-def textual_2048():
-    pass
+def read_player_command():
+    move = input("Entrez votre commande (g (gauche), d (droite), h (haut), b (bas)):")
+    L=['g','d','h','b']
+    for i in range (len(L)):
+        if move==L[i]:return move
+    return "resaisez la commande"
+
+
+
+def read_size_grid():
+    size = int(input("Entrez la taille du jeu :"))
+    return size
+
+    
+
+
+def read_theme_grid():
+    theme = input("Choisissez le thème (Default, Chemistry, Alphabet) :")
+    if theme not in ["Default", "Chemistry", "Alphabet"]:
+        return read_theme_grid()
+
+    return theme
+
+    
+
+
