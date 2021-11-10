a8d12231a9fe3bd8904e2a7a658a8f2fd763ee91
Bilal_Kopp <bilal.kopp@student-cs.fr>
2021-11-10 13:51:47 +0100

    mon travail mediocre en retard

---
+++ game2048/tutoWidget_Bilal.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+from tkinter import *
+
+# Window creation, root of the interface
+root = Tk()
+
+grille_de_jeu = Frame(root, bd=1, relief='solid')
+
+# Running of the Tkinter loop that ends when we close the windw
+root.mainloop()
